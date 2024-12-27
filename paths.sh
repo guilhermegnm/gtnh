5154c44b08ae645f28c3f3b5c82aaa26b3c78c6b
Guilherme <guilherme.gn.monteiro@gmail.com>
2024-12-27 00:43:43 +0000

    Stop tracking paths.sh

--- paths.sh
+++
@@ -1,5 +0,0 @@
-PATH_TO_MC="C:/Users/guilh/AppData/Roaming/PrismLauncher/instances/GTNH 2.7.1/.minecraft"
-PATH_TO_BACKUP="G:/O meu disco/backups/gtnh-backups"
-export PATH_TO_MC
-export PATH_TO_BACKUP
-
